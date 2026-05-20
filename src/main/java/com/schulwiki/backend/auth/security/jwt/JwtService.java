70f7ac535d872637c16a327152950d3d2e33f683
Vladyslav <148783729+w11ld@users.noreply.github.com>
2026-05-20 23:53:07 +0200

    feat: configure JWT authentication and rate limiting filters

---
+++ src/main/java/com/schulwiki/backend/auth/security/jwt/JwtService.java
@@ -0,0 +1,48 @@
+package com.schulwiki.backend.auth.security.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Getter
+public class JwtService {
+    private final SecretKey key;
+
+    private final long accessTokenExpirationMs;
+
+    public JwtService(@Value("${jwt.secret}") String secret, @Value("${jwt.expiration}") long accessTokenExpirationMs) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.accessTokenExpirationMs = accessTokenExpirationMs;
+    }
+
+    public String generateToken(Long userId, String fingerprint) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("device_fingerprint", fingerprint);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userId.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + accessTokenExpirationMs))
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims extractAllClaims(String token) throws JwtException {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
